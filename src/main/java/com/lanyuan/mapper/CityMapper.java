4ff33d863a59c1ffd291a5e3ca4285e6d54001ff
caokun <caokun@IT01010030298.kc.net>
2015-04-05 17:42:40 +0800

    提交city和wxaccount的mapper相关

---
+++ src/main/java/com/lanyuan/mapper/CityMapper.java
@@ -0,0 +1,15 @@
+package com.lanyuan.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import com.lanyuan.base.BaseMapper;
+import com.lanyuan.entity.City;
+import com.lanyuan.entity.WxAccType;
+
+public interface CityMapper extends BaseMapper<City>{
+	
+	public City isExist(int cityId);
+    
+	public City countCity(City city);
+}
