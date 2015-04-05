f5a99abfe1610f005123531c06d12c0a2a00879d
freetiger <freetiger.cn@gmail.com>
2015-04-05 18:32:53 +0800

    对外接口设计完成，可以调试。

--- src/main/java/com/lanyuan/mapper/CityMapper.java
+++ src/main/java/com/lanyuan/mapper/CityMapper.java
@@ -12,4 +12,6 @@ public interface CityMapper extends BaseMapper<City>{
 	public City isExist(int cityId);
     
 	public City countCity(City city);
+	
+	public List<City> queryCityList(Map<String, Object> map);
 }
