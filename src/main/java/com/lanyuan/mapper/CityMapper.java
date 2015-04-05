f4820a478b8add13374ae686f4953e5325b79287
caokun <caokun@IT01010030298.kc.net>
2015-04-05 19:34:38 +0800

    city mapper提交

--- src/main/java/com/lanyuan/mapper/CityMapper.java
+++ src/main/java/com/lanyuan/mapper/CityMapper.java
@@ -5,11 +5,12 @@ import java.util.Map;
 
 import com.lanyuan.base.BaseMapper;
 import com.lanyuan.entity.City;
-import com.lanyuan.entity.WxAccType;
 
 public interface CityMapper extends BaseMapper<City>{
 	
 	public City isExist(int cityId);
     
 	public City countCity(City city);
+	
+	public List<City> queryCityList(Map<String, Object> map);
 }
