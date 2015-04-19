f31e85935cbea65d789f6aa706c01134d0a966b9
freetiger <freetiger.cn@gmail.com>
2015-04-19 23:47:48 +0800

    收藏等用户行为持久化

---
+++ src/main/java/com/lanyuan/mapper/WxUserOperationMapper.java
@@ -0,0 +1,22 @@
+package com.lanyuan.mapper;
+
+import com.lanyuan.base.BaseMapper;
+import com.lanyuan.entity.WxUserOperation;
+
+public interface WxUserOperationMapper extends BaseMapper<WxUserOperation>{
+//	public WxUserOperation querySingleAccount(String wxAccountName);
+//	public WxUserOperation isExist(String wxAccountName);
+//	/**
+//	 * 验证用户登陆
+//	 * @author lanyuan
+//	 * Email：mmm333zzz520@163.com
+//	 * date：2014-2-25
+//	 * @param Account account
+//	 * @return
+//	 */
+//	public WxUserOperation countAccount(WxUserOperation account);
+//	
+//	public void addwxinfo(WxUserOperation wxAccount);
+//	
+//	public List<WxUserOperation> queryNoMatch(Map<String, Object> map);
+}
